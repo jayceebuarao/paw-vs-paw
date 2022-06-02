366a64fde44d07fcb3fcf61e2c8f3051cb4f16ab
JayceeBuarao <jayceebuarao@gmail.com>
2022-06-03 00:03:56 +0400

    add betting function, vfx, sfx

--- assets/javascript/request-api.js
+++ assets/javascript/request-api.js
@@ -5,21 +5,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function getPetData(){
     //call loading function
     //disable betting feature
+    //set images to loading visual
+
+    //reset fields
+    
+    var dogHand = document.getElementById("dog-hand");
+    var catHand = document.getElementById("cat-hand");
+    dogHand.innerHTML = "<a class='btn fs-big p-0' onclick='placeBet(\""+"dog"+ "\")' data-pet='dog'>&#128054;</a>" ;
+    catHand.innerHTML = "<a class='btn fs-big p-0' onclick='placeBet(\""+"cat"+ "\")' data-pet='cat'>&#128049;</a>";
+    
+    var userBetDiv = document.getElementById("user-bet");
+    userBetDiv.innerHTML = "None, choose your challenger!";
+    userbet = null;
+
+    var verdict = document.getElementById("verdict");
+    verdict.innerHTML = "Pick your bet!"
+    
+    var userScoreDiv = document.getElementById("user-board");
+    var verdict = document.getElementById("verdict-board");
+    verdict.classList.remove("win");
+    verdict.classList.remove("lose");
+    userScoreDiv.classList.remove("win");
+    userScoreDiv.classList.remove("lose");
+    var catScoreboard = document.getElementById("cat-scoreboard");
+    var dogScoreboard = document.getElementById("dog-scoreboard");
+    catScoreboard.classList.remove("win");
+    catScoreboard.classList.remove("lose");
+    dogScoreboard.classList.remove("win");
+    dogScoreboard.classList.remove("lose");
 
     //request images
     fetch("https://api.thedogapi.com/v1/images/search", {"method": "GET"})
     .then(response => response.json())
-    .then(data => {document.getElementById("dog-img").src = data[0].url, document.getElementById("dog-bg").style.backgroundImage = "url('"+ data[0].url + "')"})
+    .then(data => {
+        document.getElementById("dog-img").src = data[0].url;
+        document.getElementById("dog-bg").style.backgroundImage = "url('"+ data[0].url + "')";
+    })
     .catch(err => {
         console.error(err);
     })
     
     fetch("https://api.thecatapi.com/v1/images/search", {"method": "GET"})
     .then(response => response.json())
-    .then(data => {document.getElementById("cat-img").src = data[0].url; document.getElementById("cat-bg").style.backgroundImage = "url('"+ data[0].url + "')";})
+    .then(data => {
+        document.getElementById("cat-img").src = data[0].url; 
+        document.getElementById("cat-bg").style.backgroundImage = "url('"+ data[0].url + "')"; 
+    })
     .catch(err => {
         console.error(err);
     })
-
-    //enable betting feature
 }
--- assets/javascript/rock-paper-scissors.js
+++ assets/javascript/rock-paper-scissors.js
@@ -1,34 +1,54 @@
+let catScore = 0;
+let dogScore = 0;
+let userbet = null;
+let userpoint = 0;
+var result = null;
+var winsfx = new Audio("assets/sfx/609336__kenneth-cooney__completed.wav");
+var failsfx = new Audio("assets/sfx/404743__owlstorm__retro-video-game-sfx-fail.wav");
+var tiesfx = new Audio("assets/sfx/241000__dsisstudios__short-fart-01.wav");
+
 //display hand to user
 function showHand(){
     var dogHand = document.getElementById("dog-hand");
     var catHand = document.getElementById("cat-hand");
 
-    dogHand.innerHTML = rollHand();
-    catHand.innerHTML = rollHand();
+    var dogHand1 = rollHand();
+    var catHand1 = rollHand();
 
-    processResult(dogHand.innerHTML, catHand.innerHTML);
+    processResult(dogHand1, catHand1);
+
+    if(dogHand1 == "rock"){
+        dogHand.innerHTML = "🪨";
+    }else if(dogHand1 == "paper"){
+        dogHand.innerHTML = "📃";
+    }else if(dogHand1 == "scissors"){
+        dogHand.innerHTML = "✂️";
+    }
+
+    if(catHand1 == "rock"){
+        catHand.innerHTML = "🪨";
+    }else if(catHand1 == "paper"){
+        catHand.innerHTML = "📃";
+    }else if(catHand1 == "scissors"){
+        catHand.innerHTML = "✂️";
+    }
+
 }
 
-//randomize each player's hand
+//randomize each pet's hand
 function rollHand(){
     var randnum = Math.floor(Math.random() * 3);
     const hands = ["rock", "paper", "scissors"];
     return hands[randnum];
 }
 
-//count down before showing hand
-function countDown(){
-
-}
-
-let catScore = 0;
-let dogScore = 0;
-
 //process players score and users scores by analyzing bet result
 function processResult(dogHand, catHand){
     var dogScoreDiv = document.getElementById("dog-score");
     var catScoreDiv = document.getElementById("cat-score");
+    var userScoreDiv = document.getElementById("user-score");
     var verdict = document.getElementById("verdict");
+    result = null;
     if(dogHand != catHand){
         if(dogHand == "rock"){
             if(catHand == "paper"){
@@ -61,22 +81,96 @@ function processResult(dogHand, catHand){
     if (verdict.innerHTML == "CAT TEAM WINS!"){
         catScore++;
         catScoreDiv.innerHTML = catScore;
-        console.log("catscore: " + catScore);
+        console.log("catscore: " + catScore); 
+        if(userbet == "cat"){
+            userpoint++;
+            result = "win";
+        }else if(userbet == "dog"){
+            userpoint--;
+            result = "lose";
+        }
+        toggleScoreboard("cat");
     }
     if( verdict.innerHTML == "DOG TEAM WINS!"){
         dogScore++;
         dogScoreDiv.innerHTML = dogScore;
         console. log("dogscore: " + dogScore);
+        if(userbet == "dog"){
+            userpoint++;
+            result = "win";
+        }else if(userbet == "cat"){
+            userpoint--;
+            result = "lose";
+        }
+        toggleScoreboard("dog");
     }
+
+    userScoreDiv.innerHTML = userpoint; 
+    cuefx();
 }   
 
 //initialize user's bet
-function pickBet(){
-
+function placeBet(bet){
+    //get data-pet (dog/cat)
+    //set userbet = data-pet
+    // var bet = this.getAttribute("data-pet");
+    userbet = bet;
+    var userBetDiv = document.getElementById("user-bet");
+    userBetDiv.innerHTML = bet;
+    console.log("placebet() called" + bet);
 }
 
+function toggleScoreboard(winner){
+    var catScoreboard = document.getElementById("cat-scoreboard");
+    var dogScoreboard = document.getElementById("dog-scoreboard");
+    catScoreboard.classList.remove("win");
+    catScoreboard.classList.remove("lose");
+    dogScoreboard.classList.remove("win");
+    dogScoreboard.classList.remove("lose");
+    if(winner == "cat"){
+        catScoreboard.classList.add("win");
+        dogScoreboard.classList.add("lose");
+    }else if(winner == "dog"){
+        catScoreboard.classList.add("lose");
+        dogScoreboard.classList.add("win");
+    }
+}
 
-//update results from processResults()
-function updateScore(){
+function cuefx(){
+    var userScoreDiv = document.getElementById("user-board");
+    var verdict = document.getElementById("verdict-board");
+    verdict.classList.remove("win");
+    verdict.classList.remove("lose");
+    userScoreDiv.classList.remove("win");
+    userScoreDiv.classList.remove("lose");
+    if (result == "win"){
+        winsfx.play();
+        verdict.classList.add("win");
+        userScoreDiv.classList.add("win");
+    }else if (result == "lose"){
+        failsfx.play();
+        verdict.classList.add("lose");
+        userScoreDiv.classList.add("lose");
+    }else{
+        tiesfx.play();
+    }
+}
 
+function reset(){
+    catScore = 0;
+    dogScore = 0;
+    userpoint = 0 
+    userbet = null;
+    result = null;
+    var dogScoreDiv = document.getElementById("dog-score");
+    var catScoreDiv = document.getElementById("cat-score");
+    var userScoreDiv = document.getElementById("user-score");
+    var userBetDiv = document.getElementById("user-bet");
+    var verdict = document.getElementById("verdict");
+    dogScoreDiv.innerHTML = dogScore;
+    catScoreDiv.innerHTML = catScore;
+    userScoreDiv.innerHTML = userpoint;
+    userBetDiv.innerHTML = "no bets yet. pick a side :))"
+    verdict.innerHTML = "TAKE A BET!!!!"
+    cuefx();
 }
